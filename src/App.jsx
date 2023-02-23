231cb0e102b6bf4357aee2b3ea595452a6a0bd64
Samyak <samyakjain003@gmail.com>
2023-02-23 10:08:32 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
+				</a>
+			</div>
+			<h1>The Virtual Emporium</h1>
+			<div className="card">
+				<p>Website Under Construction</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
